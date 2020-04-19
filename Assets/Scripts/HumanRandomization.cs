a542eca18e336de70e8b522805b0fec02d957af7
Erik Uggeldahl <erikuggeldahl@gmail.com>
2020-04-19 04:42:37 -0700

    Added human randomization

---
+++ Assets/Scripts/HumanRandomization.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class HumanRandomization : MonoBehaviour
+{
+    [SerializeField]
+    SkinnedMeshRenderer skinRenderer = null;
+
+    [SerializeField]
+    SkinnedMeshRenderer eyesRenderer = null;
+
+    [SerializeField]
+    SkinnedMeshRenderer[] shirtRenderers = null;
+
+    [SerializeField]
+    SkinnedMeshRenderer pantsRenderer = null;
+
+    [SerializeField]
+    Material[] skin = null;
+
+    [SerializeField]
+    Material[] eyes = null;
+
+    [SerializeField]
+    Material[] shirts = null;
+
+    [SerializeField]
+    Material[] pants = null;
+
+    void Start()
+    {
+        var randomSkin = Random.Range(0, skin.Length);
+        var randomEyes = Random.Range(0, eyes.Length);
+        var randomShirt = Random.Range(0, shirts.Length);
+        var randomPants = Random.Range(0, pants.Length);
+
+        skinRenderer.material = skin[randomSkin];
+        eyesRenderer.material = eyes[randomEyes];
+        pantsRenderer.material = pants[randomPants];
+        foreach (var shirtRenderer in shirtRenderers)
+            shirtRenderer.material = shirts[randomShirt];
+    }
+}
